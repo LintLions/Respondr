c04c8ee2dc8b7c82aa61099c81897a87f4d34113
Ben Cooley <BMCooley@gmail.com>
2017-07-07 09:38:47 -0400

    ios init

---
+++ index.ios.js
@@ -0,0 +1,77 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  NavigatorIOS,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import LoginPage from './LoginPage';
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          You did it!!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+class iosBeacon extends React.Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Beacon',
+          component: LoginPage,
+        }}/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+});
+
+AppRegistry.registerComponent('iosBeacon', () => iosBeacon);
