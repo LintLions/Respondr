76defc66e217c29209f59c99b38cf3afc79117be
Jenny Cheng <jenny.e.cheng@gmail.com>
2017-07-08 09:43:15 -0400

    feat(helpRequest): build static view of helpRequest

--- HelpRequest.js
+++ HelpRequest.js
@@ -2,65 +2,55 @@
 
 import React, { Component } from 'react'
 import {
-  StyleSheet,
-  Image,
   View,
+  Text,
   TouchableHighlight,
-  ActivityIndicator,
-  ListView,
-  Text
+  StyleSheet
 } from 'react-native';
 import MapView from 'react-native-maps';
+
+// props received: userName, beaconLocation
+// props to pass down: yes/no to help request 
 
 class HelpRequest extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      loginStatus: ''
     };
-    
-    this.onLocationPressed = () => {
-    navigator.geolocation.getCurrentPosition(
-    location => {
-      var search = location.coords.latitude + ',' + location.coords.longitude;
-      this.setState({ searchString: search });
-      var query = urlForQueryAndPage('centre_point', search, 1);
-      this._executeQuery(query);
-    },
-    error => {
-      this.setState({
-        message: 'There was a problem with obtaining your location: ' + error
-      });
-    });
-
-}
+    this.handlePress = this.handlePress.bind(this);
   }
+
+  handlePress = (e) => {
+    this.setState ({
+      loginStatus: {e}
+    })
+    console.log('+++e from handlPress:', e);
+  }
+
   render() {
     return (
-      <View style={styles.map}>
-        <MapView style={styles.map}
+      <View style={styles.container}>
+        <MapView style={styles.box2}
           showsUserLocation={true}
           followsUserLocation={true}
         />
-        <View style={styles.container}>
-          <Text style={styles.description}>
-            I got props and stuff!!
-          </Text>          
-        </View>
-        <View style={styles.notification}>
-          <Text style={styles.description}>
-            Hi _____, there's a beacon on ______ street, would you be able to assist?
+        <View style={styles.box1}>
+          <Text style={styles.prompt}>
+            Hi {this.props.userName}, there's a beacon at {this.props.beaconLocation}, would you be able to assist?
           </Text>
           <View style={styles.flowRight}>
             <TouchableHighlight 
               style={styles.button}
               underlayColor='#99d9f4'
+              onPress={this.handlePress}
               >
               <Text style={styles.buttonText}>Yes</Text>
             </TouchableHighlight>
             <TouchableHighlight 
               style={styles.button}
               underlayColor='#99d9f4'
+              onPress={this.handlePress}
               >
               <Text style={styles.buttonText}>No</Text>
             </TouchableHighlight>
@@ -73,49 +63,47 @@ class HelpRequest extends Component {
 
 var styles = StyleSheet.create({
   container: {
-    padding: 30,
-    marginTop: 65,
-    alignItems: 'center'
+    flex: 1
   },
-  description: {
-    marginBottom: 20,
-    fontSize: 18,
-    textAlign: 'center',
-    color: '#656565'
+  box1: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center'
+  },
+  box2: {
+    flex: 2
   },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
-  notification: {
-    flex: 1,
-    alignItems: 'stretch',
-    // position: 'absolute',
-    // height: 100,
-    padding: 30,
-    bottom: 0,
-    backgroundColor: 'rgba(225,225,225,0.8)'
+  prompt: {
+    fontSize: 18,
+    padding: 5,
+    textAlign: 'center',
+    color: '#656565',
+    marginBottom: 20
   },
   button: {
-    height: 36,
     flex: 1,
-    flexDirection: 'row',
+    // flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+    height: 36,
+    margin: 10,
+
     backgroundColor: '#48BBEC',
     borderColor: '#48BBEC',
     borderWidth: 1,
     borderRadius: 8,
-    margin: 10,
-    alignSelf: 'stretch',
-    justifyContent: 'center'
   },
   buttonText: {
     fontSize: 18,
     color: 'white',
-    alignSelf: 'center'
   },
   flowRight: {
     flexDirection: 'row',
-    alignItems: 'center',
-    alignSelf: 'stretch'
+    // alignItems: 'center',
   }
 });
 
