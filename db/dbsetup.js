463e10d970234655b7132b28bb9566f15eba6ddb
Matthew Smith <matt.smith861@gmail.com>
2017-07-10 16:22:36 -0400

    feat(beacon button): Update beacon button to save beacon location to db
    Updates get help button to save beacon location to the database.

--- db/dbsetup.js
+++ db/dbsetup.js
@@ -1,12 +1,14 @@
-const dynamicAngel = require('../db/models/dynamicAngels.js');
-const staticAngelIndividual = require('../db/models/staticAngelIndividuals.js');
-const staticAngelStorefront = require('../db/models/staticAngelStorefronts.js');
+const dynamicAngel = require('./models/dynamicAngels.js');
+const staticAngelIndividual = require('./models/staticAngelIndividuals.js');
+const staticAngelStorefront = require('./models/staticAngelStorefronts.js');
+const beacon = require('./models/beacons.js')
 const test = require('./testData.js');
 
 
 dynamicAngel.sync({ force: true })
   .then(() => staticAngelIndividual.sync({ force: true}))
   .then(() => staticAngelStorefront.sync({ force: true}))
+  .then(() => beacon.sync({ force: true}))
   .then(() => staticAngelIndividual.bulkCreate(test.staticAngelIndividuals))
   .then(() => staticAngelStorefront.bulkCreate(test.staticAngelStorefronts))
   .then(() => dynamicAngel.bulkCreate(test.dynamicAngels))
