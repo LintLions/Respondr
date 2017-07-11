28e6168c44a51efb5f54018d7252e19db51a1711
Matthew Smith <matt.smith861@gmail.com>
2017-07-11 14:28:40 -0400

    style(beacon button): stylechanges

--- components/helpButton.js
+++ components/helpButton.js
@@ -10,7 +10,6 @@ import {
   StyleSheet
 } from 'react-native';
 import MapView from 'react-native-maps';
-import styles from './topbar/styles'
 
 class HelpButton extends Component {
   constructor(props) {
@@ -21,40 +20,83 @@ class HelpButton extends Component {
   }
   render() {
     const btnOrModal = this.props.helpButtonVisible ?
-      (<TouchableHighlight 
-          style={Styles.button}
-          underlayColor='#b22222'
-          onPress={this.props.getHelp}>
-          <Text style={styles.buttonText}>Get Help</Text>
-        </TouchableHighlight>) :
-        (<View style={Styles.button}>
-          <Text>Help is on the way</Text>
+      (<View>
+          <TouchableHighlight
+            style={styles.button}
+            underlayColor='#48BBEC'
+            onPress={this.props.getHelp}>
+            <Text style={styles.buttonText}>Get Help</Text>
+          </TouchableHighlight>
+        </View>) :
+        (<View>
+          <Text style={styles.prompt}>Help is on the way</Text>
           <TouchableHighlight 
-            style={Styles.button}
+            style={styles.button}
             underlayColor='#b22222'
             onPress={this.props.cancelHelp}>
             <Text style={styles.buttonText}>Cancel Help Request</Text>
           </TouchableHighlight>
         </View>);
     return (
-      <View >
+      <View style={styles.container}>
         {btnOrModal}
       </View>
     )    
   }    
 }
 
-var Styles = StyleSheet.create({
-
-  button: {  
+var styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  box1: {
     flex: 1,
-    // flexDirection: 'row',
+    height: 222,
+    padding: 15,
+    margin: 25,
+    justifyContent: 'center',
+    textAlign: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#999999',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)'
+  },
+  box2: {
+    flex: 2
+  },
+  bottomBox: {
+    position: 'absolute'
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  prompt: {
+    fontSize: 18,
+    padding: 5,
+    textAlign: 'center',
+    color: '#656565',
+    margin: 25
+  },
+  buttonDirection: {
+    flexDirection: 'row',
+  },
+  button: {
+    // flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+    height: 36,
+    margin: 10,
+
     backgroundColor: '#b22222',
-    borderColor: '#b22222',
+    borderColor: '#48BBEC',
     borderWidth: 1,
     borderRadius: 8,
-    margin: 10,
-  },  
-}); 
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+  }
+});
 
 module.exports = HelpButton
