b4722a32892bc107ea2d088403b488928e8491e1
Ben Cooley <BMCooley@gmail.com>
2017-07-25 22:17:20 -0400

    style(helpers): Organize Socket Logic

--- client/components/helpers.js
+++ client/components/helpers.js
@@ -6,49 +6,6 @@ import { store } from '../index';
 import { url } from './config';
 import { updateBeacon, acceptBeacon, updateUser, updateMyResponder, updateLocation, updateRoute } from '../actions/actions';
 
-export const decode = (t, e) => {
-  // transforms something like this geocFltrhVvDsEtA}ApSsVrDaEvAcBSYOS_@...
-  // to an array of coordinates
-  let n;
-  let o;
-  let u = 0;
-  let l = 0;
-  let r = 0;
-  let h = 0;
-  let i = 0;
-  let a = null;
-  const d = [];
-  const c = (10 ** (e || 5));
-
-  for (;u < t.length;) {
-    a = null;
-    h = 0;
-    i = 0;
-    do {
-      a = t.charCodeAt(u) - 63;
-      u += 1;
-      i |= (31 & a) << h;
-      h += 5;
-    }
-    while (a >= 32);
-    n = (1 & i) ? (~(i >> 1)) : i >> 1;
-    h = 0;
-    i = 0;
-    do {
-      a = t.charCodeAt(u) - 63;
-      u += 1;
-      i |= (31 & a) << h;
-      h += 5;
-    }
-    while (a >= 32);
-    o = (1 & i) ? (~(i >> 1)) : i >> 1;
-    l += n;
-    r += o;
-    d.push([l / c, r / c]);
-  }
-  return d.map(time => ({ latitude: time[0], longitude: time[1] }));
-};
-
 export const updateToken =  async (value) => {
   try {
     await AsyncStorage.setItem('id_token', value);
@@ -58,9 +15,6 @@ export const updateToken =  async (value) => {
 };
 
 export const getToken =  async () => AsyncStorage.getItem('id_token');
-
-export const socket = SocketIOClient(url);
-
 
 export const startLocationUpdate = (token) => {
   // console.log('looping')
@@ -92,6 +46,9 @@ export const startLocationUpdate = (token) => {
     };
 };
 
+// SOCKET ----------------------
+
+export const socket = SocketIOClient(url);
 socket.on('newBeacon', (currentSession) => { 
   console.log('+++helpers.js - newBeacon - currentSession: ', currentSession);
   store.dispatch(updateBeacon({
@@ -194,3 +151,48 @@ socket.on('update location', (chatroom, location) => {
   }
 });
 
+
+// google maps decoder ----------------------
+
+export const decode = (t, e) => {
+  // transforms something like this geocFltrhVvDsEtA}ApSsVrDaEvAcBSYOS_@...
+  // to an array of coordinates
+  let n;
+  let o;
+  let u = 0;
+  let l = 0;
+  let r = 0;
+  let h = 0;
+  let i = 0;
+  let a = null;
+  const d = [];
+  const c = (10 ** (e || 5));
+
+  for (;u < t.length;) {
+    a = null;
+    h = 0;
+    i = 0;
+    do {
+      a = t.charCodeAt(u) - 63;
+      u += 1;
+      i |= (31 & a) << h;
+      h += 5;
+    }
+    while (a >= 32);
+    n = (1 & i) ? (~(i >> 1)) : i >> 1;
+    h = 0;
+    i = 0;
+    do {
+      a = t.charCodeAt(u) - 63;
+      u += 1;
+      i |= (31 & a) << h;
+      h += 5;
+    }
+    while (a >= 32);
+    o = (1 & i) ? (~(i >> 1)) : i >> 1;
+    l += n;
+    r += o;
+    d.push([l / c, r / c]);
+  }
+  return d.map(time => ({ latitude: time[0], longitude: time[1] }));
+};
