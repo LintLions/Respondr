fccb7190fba5194105ce666240c3616c363cb940
Ben Cooley <BMCooley@gmail.com>
2017-07-27 13:26:34 -0400

    chore(StaicResponders): Update storefront field for pharmacy data

--- server/db/dbsetup.js
+++ server/db/dbsetup.js
@@ -3,15 +3,24 @@ require('node-env-file')(path.join(__dirname, '../../.env'));
 const dynamicResponder = require('./models/dynamicResponders.js');
 const staticResponderIndividual = require('./models/staticResponderIndividuals.js');
 const staticResponderStorefront = require('./models/staticResponderStorefronts.js');
-const beacon = require('./models/beacons.js')
-const test = require('./testData.js');
+const beacon = require('./models/beacons.js');
+const test = require('../../testData.js');
 
+
+const stores = test.map(place => ({
+  fullName: place.FacilityName,
+  street: place.Address2 ? `${place.Address} ${place.Address2}`,
+  Location: [place.latitude, place.longitude],
+  zip: place.ZipCode,
+  phone: place.Phone,
+  state: 'NY',
+  public: true,
+}));
+console.log('test', Array.isArray(stores));
 
 dynamicResponder.sync({ force: true })
-  .then(() => staticResponderIndividual.sync({ force: true}))
-  .then(() => staticResponderStorefront.sync({ force: true}))
-  .then(() => beacon.sync({ force: true}))
-  .then(() => staticResponderIndividual.bulkCreate(test.staticResponderIndividuals))
-  .then(() => staticResponderStorefront.bulkCreate(test.staticResponderStorefronts))
-  .then(() => dynamicResponder.bulkCreate(test.dynamicResponders))
+  .then(() => staticResponderIndividual.sync({ force: true }))
+  .then(() => staticResponderStorefront.sync({ force: true }))
+  .then(() => beacon.sync({ force: true }))
+  .then(() => staticResponderStorefront.bulkCreate(stores))
   .then(msg => console.log(msg));
